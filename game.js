d26183de4c764de1743bdc12eb4c5cbd64d743d4
Mayur Sahu <clientms10@gmail.com>
2023-04-08 22:00:46 +0530

    final code add

--- game.js
+++ game.js
@@ -28,9 +28,42 @@ $(".btn").click(function() {
 
     animatePress(userChosenColour);
 
+    checkAnswer(userClickedPattern.length-1);
+
 });
 
+function checkAnswer(currentLevel){
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+
+        console.log("success");
+
+        if (userClickedPattern.length === gamePattern.length){
+
+            setTimeout (function() {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        console.log("wrong");
+
+        playSound("wrong");
+
+        $("body").addClass("game-over");
+        setTimeout (function() {
+        $("body").removeClass("game-over");
+        }, 200);
+
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+
+        startOver();
+    }
+
+}
+
 function nextSequence() {
+
+    userClickedPattern = [];
 
     level++;
 
@@ -61,4 +94,11 @@ function animatePress(currentColor) {
 
         $("#" + currentColor).removeClass("pressed");
     }, 100);
+}
+
+function startOver(){
+
+    level = 0;
+    gamePattern = [];
+    started = false;
 }
